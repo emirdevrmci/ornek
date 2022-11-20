3e7d448ff19add72cfcc7ee5d14b5c2341dee046
rhapsoddyy <118104268+rhapsoddyy@users.noreply.github.com>
2022-11-20 16:28:36 +0300

    Add files via upload

---
+++ hesap makinesi.py
@@ -0,0 +1,36 @@
+a=101
+s="SENG"
+
+print(s+str(a))
+
+####
+
+a=input("Hello, please enter a number:")
+b=input("Hello, please enter another number:")
+a=float(a)
+b=int(b)
+c=a+b
+
+print(c)
+
+x=99
+####
+
+if a>b:
+    print("Maximum number is" +str(a))
+    if b>a:
+        print("Maximum number is"+ str(b)) 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
